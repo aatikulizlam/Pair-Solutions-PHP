5917421b17e00f770b28c73523028d6f5034b8cb
Atikul Islam <aatikulizlam@yahoo.com>
2013-08-13 10:45:02 +0600

    Coding style changes

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
 <?php
+/**
+ * Short description for file.
+ *
+ * PHP version 5
+ *
+ * @category   CategoryName
+ * @package    PackageName
+ * @subpackage SubPackageName
+ * @author     Atikul Islam <mail@atikul.me>
+ * @copyright  2013 Atikul.me
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
+ * @version    GIT: ***
+ * @link       URL
+ * @since      File available since Release 0.0.0
+ * @deprecated File deprecated in Release 0.0.0
+ */
 
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	
-	$fr = fopen("php://stdin", "r");
-	$fw = fopen("php://stdout", "w");
-	
-	$ary_nums = explode(" ", fgets(STDIN));
-	//$hi = (int)$ary_nums[0];
-	$low = (int)$ary_nums[1];
-	
-	$ary_nums = explode(" ", fgets(STDIN));
-	$ary_nums = array_count_values($ary_nums);
-	$count = 0;
-	foreach($ary_nums as $key=>$value){
-	    $result = $key + $low;
-	    if(array_key_exists($result, $ary_nums)){
-	        $count = $count + $ary_nums[$result];
-	    }
-	}
-	
-	fprintf($fw, "%d", $count);
+/** Enter your code here. Read input from STDIN. Print output to STDOUT. */
+
+$fR = fopen('php://stdin', 'r');
+$fW = fopen('php://stdout', 'w');
+
+$arNums = explode(' ', fgets(STDIN));
+$iLow = (int) $arNums[1];
+$arNums = explode(' ', fgets(STDIN));
+$arNums = array_count_values($arNums);
+$iCount = 0;
+
+foreach ($arNums as $iKey => $iValue) {
+    $iResult = ( $iKey + $iLow );
+    if (array_key_exists($iResult, $arNums) === true) {
+        $iCount = ( $iCount + $arNums[$iResult] );
+    }
+}
+
+fprintf($fW, '%d', $iCount);
